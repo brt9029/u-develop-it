6278eb61c211af50d2bb1371c23e5dc1bbdcc438
brt9029 <brt9029@gmail.com>
2021-12-21 13:29:18 -0600

    added database call

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // SQL Username
+        user: process.env.DB_USER,
+        // SQL Password
+        password: process.env.DB_PASS,
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
+})
+
+// Default request for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
