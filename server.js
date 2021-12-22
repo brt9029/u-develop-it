232a354778ad8bc05e43862e908b208142ead1c1
brt9029 <brt9029@gmail.com>
2021-12-22 12:07:59 -0600

    updated schema and seeds for additional data, made foreign key references to candidates from parties, and updated the sql query to use JOIN LEFT when showing results

--- server.js
+++ server.js
@@ -71,7 +71,12 @@ app.post('/api/candidate', ({ body }, res) => {
 
 // GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = 'SELECT * FROM candidates WHERE id = ?';
+    const sql = `SELECT candidates.*, parties.name
+                  AS party_name
+                  FROM candidates
+                  LEFT JOIN parties
+                  ON candidates.party_id = parties.id
+                  WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
@@ -89,7 +94,11 @@ app.get('/api/candidate/:id', (req, res) => {
 
 // GET all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                  AS party_name
+                  FROM candidates
+                  LEFT JOIN parties
+                  ON candidates.party_id = parties.id`;
 
     db.query('SELECT * FROM candidates', (err, rows) => {
         if (err) {
