3858b3c14ff7a9918c35494ee8bfef45d63672f0
brt9029 <brt9029@gmail.com>
2022-08-19 03:38:51 -0700

    added server.js functionality

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'toast333',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 app.use((req, res) => {
     res.status(404).end();
